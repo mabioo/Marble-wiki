6116547d00d71646b2133a6c545a4e3755dd37cb
marble <745376473@qq.com>
2017-08-26 15:29:00 +0800

    1.modify the question when delelte article attache and image cannot be delete !!
    2.delete the menu bar besides

--- wiki/plugins/attachments/models.py
+++ wiki/plugins/attachments/models.py
@@ -169,29 +169,29 @@ def on_revision_delete(instance, *args, **kwargs):
         return
 
     # Remove file
-    path = instance.file.path.split("/")[:-1]
+    path = instance.file.path.split("\\")[:-1]
     instance.file.delete(save=False)
 
     # Clean up empty directories
 
     # Check for empty folders in the path. Delete the first two.
-    # if len(path[-1]) == 32:
-    #     # Path was (most likely) obscurified so we should look 2 levels down
-    #     max_depth = 2
-    # else:
-    #     max_depth = 1
-    # for depth in range(0, max_depth):
-    #     delete_path = "/".join(path[:-depth] if depth > 0 else path)
-    #     try:
-    #         if len(
-    #             os.listdir(
-    #                 os.path.join(
-    #                     django_settings.MEDIA_ROOT,
-    #                     delete_path))) == 0:
-    #             os.rmdir(delete_path)
-    #     except OSError:
-    #         # Raised by os.listdir if directory is missing
-    #         pass
+    if len(path[-1]) == 32:
+        # Path was (most likely) obscurified so we should look 2 levels down
+        max_depth = 2
+    else:
+        max_depth = 1
+    for depth in range(0, max_depth):
+        delete_path = "/".join(path[:-depth] if depth > 0 else path)
+        try:
+            if len(
+                os.listdir(
+                    os.path.join(
+                        django_settings.MEDIA_ROOT,
+                        delete_path))) == 0:
+                os.rmdir(delete_path)
+        except OSError:
+            # Raised by os.listdir if directory is missing
+            pass
 
 
 @disable_signal_for_loaddata
